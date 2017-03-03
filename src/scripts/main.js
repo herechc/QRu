57822ef0abb4682eaacab52083f79037a2b87d67
herechc <hanlike721vip@sina.com>
2017-03-03 17:30:33 +0800

    by herec

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,11 @@
 $(function () {
-
+    $('#owl-carousel').owlCarousel({
+        autoplay:false,
+        dots: true,
+        dotsSpeed:400,
+        items:1
+    });
+    $(".page").fullpage({
+        verticalCentered: false
+    })
 });
