20d2e073996e478045df42a139cd4aac98a593e5
nonzar <dwljia@qq.com>
2017-03-22 02:13:47 +0800

    edit by nz

--- src/scripts/templater.js
+++ src/scripts/templater.js
@@ -1,7 +1,20 @@
+console.log(document.querySelector("html").clientHeight)
+$(".owl-carousel .slider").height(document.querySelector("html").clientHeight)
 $(function () {
     $("#fullpage").fullpage({
         verticalCentered: true,
         responsiveWidth: 0,
+        afterLoad: function (anchorLink, index) {
+            if (index != 1) {
+                $(".navbar").removeClass("opacity");
+            } else {
+                $(".navbar").addClass("opacity");
+            }
+        }
     });
-    $.fn.fullpage.moveTo(4);
+    $.fn.fullpage.moveTo(1);
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true
+    })
 });
